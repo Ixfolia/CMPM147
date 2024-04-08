0ca5866a5c38483cf420e26b7bb77074e928792a
Brady Lin <linbrady16877@gmail.com>
2024-04-08 00:57:51 -0700

    Copied from Glitch to here

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -7,28 +7,66 @@
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
+function main() {
+  const fillers = {
+    adventurer: ["adventurer", "traveller"],
+    pre: ["Fra", "Tro", "Gre", "Hor"],
+    post: ["gria", "ston", "gott", "kin", "dar"],
+    item_adj: ["fire", "water", "light", "arcane"],
+    item: ["axe", "staff", "book", "cloak", "sword", "boots"],
+    num: [
+      "two",
+      "three",
+      "four",
+      "five",
+      "six",
+      "seven",
+      "ten",
+      "eleven",
+      "twelve",
+    ],
+    looty: ["copper", "silver", "gold"],
+    loots: ["coins", "chalices", "ingots"],
+    baddies: ["orcs", "glubs", "fishmen", "goblins", "ogres", "wolves"],
+    place: ["town", "city", "outskirts", "village"],
+  };
+  
+  const template = `The $place of $pre$post is under attack by $num $baddies!
+
+  Rewards: $num $looty $loots, $item_adj $item
+  
+  
+  `;
+
+  // STUDENTS: You don't need to edit code below this line.
+  
+  const slotPattern = /\$(\w+)/;
+  
+  function replacer(match, name) {
+    let options = fillers[name];
+    if (options) {
+      return options[Math.floor(Math.random() * options.length)];
+    } else {
+      return `<UNKNOWN:${name}>`;
+    }
   }
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+  function generate() {
+    let story = template;
+    while (story.match(slotPattern)) {
+      story = story.replace(slotPattern, replacer);
+    }
+  
+    /* global box */
+    $("#box").text(story);
   }
-}
-
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
-
-  // call a method on the instance
-  myInstance.myMethod();
+  
+  /* global clicker */
+  $("#clicker").click(generate);
+  
+  generate();
+  
 }
 
 // let's get this party started - uncomment me
-//main();
+main();
